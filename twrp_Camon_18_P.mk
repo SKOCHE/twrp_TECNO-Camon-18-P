c00c25cd7dda63a148917c345ca2c0f84749b984
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-05-07 18:05:58 +0700

    Update twrp_Camon_18_P.mk

--- twrp_Camon_18_P.mk
+++ twrp_Camon_18_P.mk
@@ -12,17 +12,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from TECNO Camon 18 P device
+# Inherit from TECNO Camon-18-P device
 $(call inherit-product, device/Tecno/Camon 18 P/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Camon 18 P
-PRODUCT_NAME := twrp_Camon 18 P
+PRODUCT_DEVICE := Camon-18-P
+PRODUCT_NAME := twrp_Camon-18-P
 PRODUCT_BRAND := TECNO
-PRODUCT_MODEL := Comon 18 P
+PRODUCT_MODEL := Camon-18-P
 PRODUCT_MANUFACTURER := Tecno
 PRODUCT_RELEASE_NAME := Tecno Comon 18 P
 
