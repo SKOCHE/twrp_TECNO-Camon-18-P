d90b7bc631ff226c2f7e245d28c6e2112198e571
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-05-10 10:20:24 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,10 +173,6 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 TARGET_RECOVERY_INITRC := $(DEVICE_PATH)/recovery/root/init.recovery.mt6781.rc
 
-# Partitions (listed in the file) to be wiped under recovery.
-TARGET_RECOVERY_WIPE := $          ( DEVICE
-
-
 # Recovery
 #TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
