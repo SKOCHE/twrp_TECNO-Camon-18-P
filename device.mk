5ccbb8d1f83942ddd1db54b3776b7886f387cd83
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-05-16 15:57:27 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,21 +21,15 @@ LOCAL_PATH := device/Tecno/Camon18P
 AB_OTA_PARTITIONS += \
     boot \
     dtbo \
-    lk \
-    preloader \
-    product \
     system \
+    system_ext \
+    product \
+    vendor \
+    odm \
     vbmeta \
     vbmeta_system \
-    vbmeta_vendor \
-    vendor \
+    vbmeta_vendor
     
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
 # V A/B
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
@@ -46,37 +40,31 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 31
 
-# Health Hal
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service \
-    libhealthd.$(PRODUCT_PLATFORM)
-
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl.recovery \
-    android.hardware.boot@1.1-mtkimpl \
+    android.hardware.boot@1.1-impl-recovery \
+    android.hardware.boot@1.1-impl \
     android.hardware.boot@1.1-service
-    
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-
-# MTK PlPath Utils
-PRODUCT_PACKAGES += \
-    mtk_plpath_utils.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
-    cppreopts.sh \
     update_engine \
     update_verifier \
     update_engine_sideload
+    
+# Fastbootd stuff
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+# health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
 
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
