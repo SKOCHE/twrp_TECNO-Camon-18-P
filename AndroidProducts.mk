268d905331d848e5770eff009bfaa25dc17cb786
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-05-17 16:56:38 +0700

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_TECNO-CH7n.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_TECNO-CH7n-user \
+    twrp_TECNO-CH7n-userdebug \
+    twrp_TECNO-CH7n-eng
---
+++ twrp_TECNO-CH7n.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from TECNO-CH7n device
+$(call inherit-product, device/tecno/TECNO-CH7n/device.mk)
+
+PRODUCT_DEVICE := TECNO-CH7n
+PRODUCT_NAME := twrp_TECNO-CH7n
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO CH7n
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_ch7n_h812-user 12 SP1A.210812.016 197442 release-keys"
+
+BUILD_FINGERPRINT := TECNO/CH7n-OP/TECNO-CH7n:12/SP1A.210812.016/221221V364:user/release-keys
