c6856f4f536330958582190b8162bcb7c0a3d456
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-05-19 06:31:30 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,12 +6,6 @@
 #
 
 LOCAL_PATH := device/tecno/TECNO_CH7n
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
