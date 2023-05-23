f771eb3fda0db8761aed79befcd87cd0e3ed4176
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-05-24 06:42:43 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -68,9 +68,9 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-mtkimpl.recovery \
-    android.hardware.boot@1.0-mtkimpl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.1-mtkimpl.recovery \
+    android.hardware.boot@1.1-mtkimpl \
+    android.hardware.boot@1.1-service
     
 PRODUCT_PACKAGES_DEBUG += \
     bootctrl
