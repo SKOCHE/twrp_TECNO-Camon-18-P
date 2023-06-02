626f20f4f968198bfc66fe89b8c56a9dd1a27311
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-06-02 13:27:38 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -71,6 +71,7 @@ PRODUCT_PACKAGES += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service
     
 PRODUCT_PACKAGES_DEBUG += \
