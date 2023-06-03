5b9f51b521d5d9a674c33dfac973546d6c4bb158
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-06-03 18:35:18 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,8 +177,7 @@ TW_EXCLUDE_APEX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1200
-TW_NO_SCREEN_BLANK := true
-TW_SCREEN_BLANK_ON_BOOT := true						  
+TW_NO_SCREEN_BLANK := true					  
 TW_Y_OFFSET := 50
 TW_H_OFFSET := -50							   
 
