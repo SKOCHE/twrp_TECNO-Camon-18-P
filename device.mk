4da47f7b67d93b89410cd19a5f710e3017901c3f
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-06-04 22:04:50 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,45 +16,67 @@
 
 LOCAL_PATH := device/Tecno/Camon18P
 
-# Dynamic Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Api level
-PRODUCT_SHIPPING_API_LEVEL := 31
-
-# Platform
-PRODUCT_PLATFORM := mt6781
-
 # A/B
-ENABLE_VIRTUAL_AB := true
-
-# fastboot/d hal
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-
-# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    lk \
+    preloader \
+    product \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor \
+    vendor \
+    vendor_boot
+    
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
+
+# V A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Dynam
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    libhealthd.$(PRODUCT_PLATFORM)
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-service \
-    android.hardware.boot@1.2-mtkimpl.recovery 
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-service
+    
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl
 
+# Fastbootd
 PRODUCT_PACKAGES += \
-    bootctrl.mt6781
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
 
+# MTK PlPath Utils
 PRODUCT_PACKAGES += \
-    bootctrl.mt6781 \
-    libgptutils \
-    libz \
-    libcutils
+    mtk_plpath_utils.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -63,7 +85,5 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
-# Health Hal
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+TW_OVERRIDE_SYSTEM_PROPS := \
+    "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
