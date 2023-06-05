3f2304c37065762bd90cbb5fc228ee86daa969db
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-06-05 19:53:15 +0700

    Update twrp_Camon18P.mk

--- twrp_Camon18P.mk
+++ twrp_Camon18P.mk
@@ -25,7 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
-	$(LOCAL_PATH)/prebuilt/dtb:dtb.img
+	$(LOCAL_PATH)/prebuilt/dtb.img
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Camon18P
