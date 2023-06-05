c06e7d7872f6cfae8dcf36fcb3e12f0ec2ed3937
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-06-05 12:21:41 +0700

    Add files via upload

--- device.mk
+++ device.mk
@@ -1,18 +1,19 @@
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 TeamWin Recovery Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 LOCAL_PATH := device/Tecno/Camon18P
 
@@ -52,14 +53,14 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Health Hal
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
     libhealthd.$(PRODUCT_PLATFORM)
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-service
     
 PRODUCT_PACKAGES_DEBUG += \
     bootctrl
