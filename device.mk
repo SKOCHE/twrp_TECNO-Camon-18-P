c6ccc394fa6efe6b1bd57807274bc34e30e46e5a
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-06-13 22:49:52 +0700

    Add files via upload

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ AB_OTA_PARTITIONS += \
     
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
@@ -46,43 +46,22 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_TARGET_VNDK_VERSION := 31
 
 # API
-PRODUCT_SHIPPING_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := 31
 
 
-
-# Health Hal
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service \
-    libhealthd.$(PRODUCT_PLATFORM)
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-service \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
-    
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl \
-    update_engine_client
+    android.hardware.boot@1.0-impl.recovery
 
-PRODUCT_PACKAGES += \
-    bootctrl.mt6781 \
-    bootctrl.mt6781.recovery
-
-# Fastbootd
+# fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
 
 # MTK PlPath Utils
 PRODUCT_PACKAGES += \
     mtk_plpath_utils.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
